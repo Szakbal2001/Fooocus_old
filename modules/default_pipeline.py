329a9fae41772e81fbf6dc8d43915c9abfb2c359
thekingbalXD <130457763+thekingbalXD@users.noreply.github.com>
2023-10-12 15:58:07 +0200

    Add files via upload

---
+++ modules/default_pipeline.py
@@ -0,0 +1,259 @@
+import modules.core as core
+import os
+import torch
+import modules.path
+import comfy.model_management
+
+from comfy.model_patcher import ModelPatcher
+from comfy.model_base import SDXL, SDXLRefiner
+from modules.expansion import FooocusExpansion
+
+
+xl_base: core.StableDiffusionModel = None
+xl_base_hash = ''
+
+xl_base_patched: core.StableDiffusionModel = None
+xl_base_patched_hash = ''
+
+xl_refiner: ModelPatcher = None
+xl_refiner_hash = ''
+
+final_expansion = None
+final_unet = None
+final_clip = None
+final_vae = None
+final_refiner = None
+
+loaded_ControlNets = {}
+
+
+@torch.no_grad()
+@torch.inference_mode()
+def refresh_controlnets(model_paths):
+    global loaded_ControlNets
+    cache = {}
+    for p in model_paths:
+        if p is not None:
+            if p in loaded_ControlNets:
+                cache[p] = loaded_ControlNets[p]
+            else:
+                cache[p] = core.load_controlnet(p)
+    loaded_ControlNets = cache
+    return
+
+
+@torch.no_grad()
+@torch.inference_mode()
+def assert_model_integrity():
+    error_message = None
+
+    if xl_base is None:
+        error_message = 'You have not selected SDXL base model.'
+
+    if xl_base_patched is None:
+        error_message = 'You have not selected SDXL base model.'
+
+    if not isinstance(xl_base.unet.model, SDXL):
+        error_message = 'You have selected base model other than SDXL. This is not supported yet.'
+
+    if not isinstance(xl_base_patched.unet.model, SDXL):
+        error_message = 'You have selected base model other than SDXL. This is not supported yet.'
+
+    if xl_refiner is not None:
+        if not isinstance(xl_refiner.model, SDXLRefiner):
+            error_message = 'You have selected refiner model other than SDXL refiner. This is not supported yet.'
+
+    if error_message is not None:
+        raise NotImplementedError(error_message)
+
+    return True
+
+
+@torch.no_grad()
+@torch.inference_mode()
+def refresh_base_model(name):
+    global xl_base, xl_base_hash, xl_base_patched, xl_base_patched_hash
+
+    filename = os.path.abspath(os.path.realpath(os.path.join(modules.path.modelfile_path, name)))
+    model_hash = filename
+
+    if xl_base_hash == model_hash:
+        return
+
+    xl_base = None
+    xl_base_hash = ''
+    xl_base_patched = None
+    xl_base_patched_hash = ''
+
+    xl_base = core.load_model(filename)
+    xl_base_hash = model_hash
+    print(f'Base model loaded: {model_hash}')
+    return
+
+
+@torch.no_grad()
+@torch.inference_mode()
+def refresh_refiner_model(name):
+    global xl_refiner, xl_refiner_hash
+
+    filename = os.path.abspath(os.path.realpath(os.path.join(modules.path.modelfile_path, name)))
+    model_hash = filename
+
+    if xl_refiner_hash == model_hash:
+        return
+
+    xl_refiner = None
+    xl_refiner_hash = ''
+
+    if name == 'None':
+        print(f'Refiner unloaded.')
+        return
+
+    xl_refiner = core.load_unet_only(filename)
+    xl_refiner_hash = model_hash
+    print(f'Refiner model loaded: {model_hash}')
+    return
+
+
+@torch.no_grad()
+@torch.inference_mode()
+def refresh_loras(loras):
+    global xl_base, xl_base_patched, xl_base_patched_hash
+    if xl_base_patched_hash == str(loras):
+        return
+
+    model = xl_base
+    for name, weight in loras:
+        if name == 'None':
+            continue
+
+        if os.path.exists(name):
+            filename = name
+        else:
+            filename = os.path.join(modules.path.lorafile_path, name)
+
+        assert os.path.exists(filename), 'Lora file not found!'
+
+        model = core.load_sd_lora(model, filename, strength_model=weight, strength_clip=weight)
+    xl_base_patched = model
+    xl_base_patched_hash = str(loras)
+    print(f'LoRAs loaded: {xl_base_patched_hash}')
+
+    return
+
+
+@torch.no_grad()
+@torch.inference_mode()
+def clip_encode_single(clip, text, verbose=False):
+    cached = clip.fcs_cond_cache.get(text, None)
+    if cached is not None:
+        if verbose:
+            print(f'[CLIP Cached] {text}')
+        return cached
+    tokens = clip.tokenize(text)
+    result = clip.encode_from_tokens(tokens, return_pooled=True)
+    clip.fcs_cond_cache[text] = result
+    if verbose:
+        print(f'[CLIP Encoded] {text}')
+    return result
+
+
+@torch.no_grad()
+@torch.inference_mode()
+def clip_encode(texts, pool_top_k=1):
+    global final_clip
+
+    if final_clip is None:
+        return None
+    if not isinstance(texts, list):
+        return None
+    if len(texts) == 0:
+        return None
+
+    cond_list = []
+    pooled_acc = 0
+
+    for i, text in enumerate(texts):
+        cond, pooled = clip_encode_single(final_clip, text)
+        cond_list.append(cond)
+        if i < pool_top_k:
+            pooled_acc += pooled
+
+    return [[torch.cat(cond_list, dim=1), {"pooled_output": pooled_acc}]]
+
+
+@torch.no_grad()
+@torch.inference_mode()
+def clear_all_caches():
+    xl_base.clip.fcs_cond_cache = {}
+    xl_base_patched.clip.fcs_cond_cache = {}
+
+
+@torch.no_grad()
+@torch.inference_mode()
+def prepare_text_encoder(async_call=True):
+    if async_call:
+        # TODO: make sure that this is always called in an async way so that users cannot feel it.
+        pass
+    assert_model_integrity()
+    comfy.model_management.load_models_gpu([final_clip.patcher, final_expansion.patcher])
+    return
+
+
+@torch.no_grad()
+@torch.inference_mode()
+def refresh_everything(refiner_model_name, base_model_name, loras):
+    global final_unet, final_clip, final_vae, final_refiner, final_expansion
+
+    refresh_refiner_model(refiner_model_name)
+    refresh_base_model(base_model_name)
+    refresh_loras(loras)
+    assert_model_integrity()
+
+    final_unet, final_clip, final_vae, final_refiner = \
+        xl_base_patched.unet, xl_base_patched.clip, xl_base_patched.vae, xl_refiner
+
+    if final_expansion is None:
+        final_expansion = FooocusExpansion()
+
+    prepare_text_encoder(async_call=True)
+    clear_all_caches()
+    return
+
+
+refresh_everything(
+    refiner_model_name=modules.path.default_refiner_model_name,
+    base_model_name=modules.path.default_base_model_name,
+    loras=[(modules.path.default_lora_name, 0.5), ('None', 0.5), ('None', 0.5), ('None', 0.5), ('None', 0.5)]
+)
+
+
+@torch.no_grad()
+@torch.inference_mode()
+def process_diffusion(positive_cond, negative_cond, steps, switch, width, height, image_seed, callback, sampler_name, scheduler_name, latent=None, denoise=1.0, tiled=False, cfg_scale=7.0):
+    if latent is None:
+        empty_latent = core.generate_empty_latent(width=width, height=height, batch_size=1)
+    else:
+        empty_latent = latent
+
+    sampled_latent = core.ksampler(
+        model=final_unet,
+        refiner=final_refiner,
+        positive=positive_cond,
+        negative=negative_cond,
+        latent=empty_latent,
+        steps=steps, start_step=0, last_step=steps, disable_noise=False, force_full_denoise=True,
+        seed=image_seed,
+        denoise=denoise,
+        callback_function=callback,
+        cfg=cfg_scale,
+        sampler_name=sampler_name,
+        scheduler=scheduler_name,
+        refiner_switch=switch
+    )
+
+    decoded_latent = core.decode_vae(vae=final_vae, latent_image=sampled_latent, tiled=tiled)
+    images = core.pytorch_to_numpy(decoded_latent)
+
+    comfy.model_management.soft_empty_cache()
+    return images
---
+++ modules/launch_util.py
@@ -0,0 +1,156 @@
+import os
+import importlib
+import importlib.util
+import shutil
+import subprocess
+import sys
+import re
+import logging
+import pygit2
+
+
+pygit2.option(pygit2.GIT_OPT_SET_OWNER_VALIDATION, 0)
+
+logging.getLogger("torch.distributed.nn").setLevel(logging.ERROR)  # sshh...
+logging.getLogger("xformers").addFilter(lambda record: 'A matching Triton is not available' not in record.getMessage())
+
+re_requirement = re.compile(r"\s*([-_a-zA-Z0-9]+)\s*(?:==\s*([-+_.a-zA-Z0-9]+))?\s*")
+
+python = sys.executable
+default_command_live = (os.environ.get('LAUNCH_LIVE_OUTPUT') == "1")
+index_url = os.environ.get('INDEX_URL', "")
+
+modules_path = os.path.dirname(os.path.realpath(__file__))
+script_path = os.path.dirname(modules_path)
+dir_repos = "repositories"
+
+
+def onerror(func, path, exc_info):
+    import stat
+    if not os.access(path, os.W_OK):
+        os.chmod(path, stat.S_IWUSR)
+        func(path)
+    else:
+        raise 'Failed to invoke "shutil.rmtree", git management failed.'
+
+
+def git_clone(url, dir, name, hash=None):
+    try:
+        try:
+            repo = pygit2.Repository(dir)
+            remote_url = repo.remotes['origin'].url
+            if remote_url != url:
+                print(f'{name} exists but remote URL will be updated.')
+                del repo
+                raise url
+            else:
+                print(f'{name} exists and URL is correct.')
+        except:
+            if os.path.isdir(dir) or os.path.exists(dir):
+                shutil.rmtree(dir, onerror=onerror)
+            os.makedirs(dir, exist_ok=True)
+            repo = pygit2.clone_repository(url, dir)
+            print(f'{name} cloned from {url}.')
+
+        remote = repo.remotes['origin']
+        remote.fetch()
+
+        commit = repo.get(hash)
+
+        repo.checkout_tree(commit, strategy=pygit2.GIT_CHECKOUT_FORCE)
+        repo.set_head(commit.id)
+
+        print(f'{name} checkout finished for {hash}.')
+    except Exception as e:
+        print(f'Git clone failed for {name}: {str(e)}')
+
+
+def repo_dir(name):
+    return os.path.join(script_path, dir_repos, name)
+
+
+def is_installed(package):
+    try:
+        spec = importlib.util.find_spec(package)
+    except ModuleNotFoundError:
+        return False
+
+    return spec is not None
+
+
+def run(command, desc=None, errdesc=None, custom_env=None, live: bool = default_command_live) -> str:
+    if desc is not None:
+        print(desc)
+
+    run_kwargs = {
+        "args": command,
+        "shell": True,
+        "env": os.environ if custom_env is None else custom_env,
+        "encoding": 'utf8',
+        "errors": 'ignore',
+    }
+
+    if not live:
+        run_kwargs["stdout"] = run_kwargs["stderr"] = subprocess.PIPE
+
+    result = subprocess.run(**run_kwargs)
+
+    if result.returncode != 0:
+        error_bits = [
+            f"{errdesc or 'Error running command'}.",
+            f"Command: {command}",
+            f"Error code: {result.returncode}",
+        ]
+        if result.stdout:
+            error_bits.append(f"stdout: {result.stdout}")
+        if result.stderr:
+            error_bits.append(f"stderr: {result.stderr}")
+        raise RuntimeError("\n".join(error_bits))
+
+    return (result.stdout or "")
+
+
+def run_pip(command, desc=None, live=default_command_live):
+    try:
+        index_url_line = f' --index-url {index_url}' if index_url != '' else ''
+        return run(f'"{python}" -m pip {command} --prefer-binary{index_url_line}', desc=f"Installing {desc}",
+                   errdesc=f"Couldn't install {desc}", live=live)
+    except Exception as e:
+        print(e)
+        print(f'CMD Failed {desc}: {command}')
+        return None
+
+
+def requirements_met(requirements_file):
+    """
+    Does a simple parse of a requirements.txt file to determine if all rerqirements in it
+    are already installed. Returns True if so, False if not installed or parsing fails.
+    """
+
+    import importlib.metadata
+    import packaging.version
+
+    with open(requirements_file, "r", encoding="utf8") as file:
+        for line in file:
+            if line.strip() == "":
+                continue
+
+            m = re.match(re_requirement, line)
+            if m is None:
+                return False
+
+            package = m.group(1).strip()
+            version_required = (m.group(2) or "").strip()
+
+            if version_required == "":
+                continue
+
+            try:
+                version_installed = importlib.metadata.version(package)
+            except Exception:
+                return False
+
+            if packaging.version.parse(version_required) != packaging.version.parse(version_installed):
+                return False
+
+    return True
