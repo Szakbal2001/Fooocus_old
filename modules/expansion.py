e58e53ba92495058b9c0aff018739a6d53740ab6
thekingbalXD <130457763+thekingbalXD@users.noreply.github.com>
2023-10-30 22:09:30 +0100

    Update expansion.py

--- modules/expansion.py
+++ modules/expansion.py
@@ -1,10 +1,14 @@
 import torch
+import math
+import fcbh.model_management as model_management
 
-import comfy.model_management as model_management
-
+from transformers.generation.logits_process import LogitsProcessorList
 from transformers import AutoTokenizer, AutoModelForCausalLM, set_seed
 from modules.path import fooocus_expansion_path
-from comfy.model_patcher import ModelPatcher
+from fcbh.model_patcher import ModelPatcher
+
+# limitation of np.random.seed(), called from transformers.set_seed()
+SEED_LIMIT_NUMPY = 2**32
 
 
 fooocus_magic_split = [
@@ -12,6 +16,20 @@ fooocus_magic_split = [
     ', intricate,',
 ]
 dangrous_patterns = '[]【】()（）|:：'
+
+black_list = ['art', 'digital', 'paint', 'painting', 'painted', 'drawing', 'draw', 'drawn',
+              'concept', 'illustration', 'illustrated', 'illustrate',
+              'face', 'eye', 'eyes', 'hand', 'hands', 'head', 'heads', 'leg', 'legs', 'arm', 'arms',
+              'shoulder', 'shoulders', 'body', 'facial', 'skin', 'character', 'human', 'portrait', 'cloth'
+              'monster', 'artistic', 'oil', 'brush',
+              'artwork', 'artworks',
+              'skeletal', 'skeleton', 'a', 'the', 'background']
+
+black_list += ['Ġ' + k for k in black_list]
+black_list += [k.upper() for k in black_list]
+black_list += [k.capitalize() for k in black_list]
+black_list += ['Ġ' + k.upper() for k in black_list]
+black_list += ['Ġ' + k.capitalize() for k in black_list]
 
 
 def safe_str(x):
@@ -30,6 +48,14 @@ def remove_pattern(x, pattern):
 class FooocusExpansion:
     def __init__(self):
         self.tokenizer = AutoTokenizer.from_pretrained(fooocus_expansion_path)
+        self.vocab = self.tokenizer.vocab
+        self.logits_bias = torch.zeros((1, len(self.vocab)), dtype=torch.float32)
+        self.logits_bias[0, self.tokenizer.eos_token_id] = - 16.0
+        self.logits_bias[0, 198] = - 1024.0  # test_198 = self.tokenizer('\n', return_tensors="pt")
+        for k, v in self.vocab.items():
+            if k in black_list:
+                self.logits_bias[0, v] = - 1024.0
+
         self.model = AutoModelForCausalLM.from_pretrained(fooocus_expansion_path)
         self.model.eval()
 
@@ -49,12 +75,19 @@ class FooocusExpansion:
         self.patcher = ModelPatcher(self.model, load_device=load_device, offload_device=offload_device)
         print(f'Fooocus Expansion engine loaded for {load_device}, use_fp16 = {use_fp16}.')
 
+    def logits_processor(self, input_ids, scores):
+        self.logits_bias = self.logits_bias.to(scores)
+        return scores + self.logits_bias
+
     def __call__(self, prompt, seed):
+        if prompt == '':
+            return ''
+
         if self.patcher.current_device != self.patcher.load_device:
             print('Fooocus Expansion loaded by itself.')
             model_management.load_model_gpu(self.patcher)
 
-        seed = int(seed)
+        seed = int(seed) % SEED_LIMIT_NUMPY
         set_seed(seed)
         origin = safe_str(prompt)
         prompt = origin + fooocus_magic_split[seed % len(fooocus_magic_split)]
@@ -63,12 +96,19 @@ class FooocusExpansion:
         tokenized_kwargs.data['input_ids'] = tokenized_kwargs.data['input_ids'].to(self.patcher.load_device)
         tokenized_kwargs.data['attention_mask'] = tokenized_kwargs.data['attention_mask'].to(self.patcher.load_device)
 
+        current_token_length = int(tokenized_kwargs.data['input_ids'].shape[1])
+        max_token_length = 75 * int(math.ceil(float(current_token_length) / 75.0))
+        max_new_tokens = max_token_length - current_token_length
+
+        logits_processor = LogitsProcessorList([self.logits_processor])
+
         # https://huggingface.co/blog/introducing-csearch
         # https://huggingface.co/docs/transformers/generation_strategies
         features = self.model.generate(**tokenized_kwargs,
                                        num_beams=1,
-                                       max_new_tokens=256,
-                                       do_sample=True)
+                                       max_new_tokens=max_new_tokens,
+                                       do_sample=True,
+                                       logits_processor=logits_processor)
 
         response = self.tokenizer.batch_decode(features, skip_special_tokens=True)
         result = response[0][len(origin):]
